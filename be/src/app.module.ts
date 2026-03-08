e1c82137c483d53e88fcd7132ad804b4339f1a46
trantu1504 <tranngoctu10a1xp@gmail.com>
2026-03-08 22:39:39 +0700

    Resolve merge conflicts in app.module.ts

--- be/src/app.module.ts
+++ be/src/app.module.ts
@@ -4,31 +4,12 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { UsersModule } from './modules/users/users.module'; 
-import { ShopsModule } from './modules/shops/shops.module';
-require('dotenv').config();
-
-const password = process.env.DB_PASSWORD || '';
-=======
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
 require('dotenv').config();
 
-const password = process.env.DB_PASSWORD  || '';
->>>>>>> 51bed1d574c6d496c739f49c1df00c8330a9eb0e
-=======
-=======
-import { AuthModule } from './modules/auth/auth.module';
-import { UsersModule } from './modules/users/users.module';
->>>>>>> 4312d6179b418c70d7d5f75da24fd76d5ae06a21
-require('dotenv').config();
-
 const password = process.env.DB_PASSWORD || '';
 
->>>>>>> fdb4d5abb562648b4a033b69707b06497a388cd6
 
 @Module({
   imports: [
@@ -42,13 +23,8 @@ const password = process.env.DB_PASSWORD || '';
       autoLoadEntities: true,
       synchronize: true,
     }),
-<<<<<<< HEAD
-    UsersModule,
-    ShopsModule,
-=======
     AuthModule,  
     UsersModule,
->>>>>>> 51bed1d574c6d496c739f49c1df00c8330a9eb0e
   ],
 })
 export class AppModule { };
