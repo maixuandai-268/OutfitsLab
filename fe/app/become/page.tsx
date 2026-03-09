821230b579642d3795ec7b69ae93c8cc77f34d6f
Nguyễn Đức Duyệt <duyetnguyen170104@gmail.com>
2026-03-09 15:37:38 +0700

    duyet

--- fe/app/become/page.tsx
+++ fe/app/become/page.tsx
@@ -2,13 +2,14 @@
 import { useState } from "react";
 import StepInfo from "@/components/seller/steps/StepInfo";
 import StepStore from "@/components/seller/steps/StepStore";
+import StepConfirm from "@/components/seller/steps/StepConfirm";
 
 export default function BecomPage() {
     const [name, setName] = useState("");
     const [formData, setFormData] = useState({ name: "" });
     return (
         <div className="min-h-screen bg-[#FFFBF5]">
-            <StepStore name={""} />
+            <StepConfirm name={""} />
         </div>
     )
 }
