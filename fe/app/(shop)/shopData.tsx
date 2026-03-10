3fa97029e97aef61c628926622261ee0c2b64775
qtrunghd12 <nsnd1357@gmail.com>
2026-03-10 21:57:05 +0700

    Product Info UI

--- fe/app/(shop)/shopData.tsx
+++ fe/app/(shop)/shopData.tsx
@@ -31,6 +31,9 @@ export interface Product {
   price: number;
   image_url: string;
   model_3d_url: string;
+  sold: number;
+  rating: number;
+  reviews: number;
 }
 
 export interface Order {
@@ -70,21 +73,18 @@ export const SHOPS: Shop[] = [
 ];
 
 export const PRODUCTS: Product[] = [
-  { id: 1, shop_id: 1, type: 'TOP', gender: 'Male', name: 'Retro Graphic Tee', price: 350000, image_url: 'https://images.unsplash.com/photo-1583743814966-8936f5b7be1a?auto=format&fit=crop&w=500&q=80', model_3d_url: '#' },
-  { id: 2, shop_id: 1, type: 'BOTTOM', gender: 'Male', name: 'Baggy Jeans 90s', price: 5500000, image_url: 'https://images.unsplash.com/photo-1542272454315-4c01d7abdf4a?auto=format&fit=crop&w=500&q=80', model_3d_url: '#' },
-  { id: 3, shop_id: 1, type: 'SHOES', gender: 'Male', name: 'Classic Chunky Sneakers', price: 1200000, image_url: 'https://images.unsplash.com/photo-1542291026-7eec264c27ff?auto=format&fit=crop&w=500&q=80', model_3d_url: '#' },
-  { id: 4, shop_id: 2, type: 'TOP', gender: 'Male', name: 'Black Oversized Hoodie', price: 650000, image_url: 'https://images.unsplash.com/photo-1556905055-8f358a7a47b2?auto=format&fit=crop&w=500&q=80', model_3d_url: '#' },
-  { id: 5, shop_id: 2, type: 'BOTTOM', gender: 'Male', name: 'Cargo Pants Army Green', price: 550000, image_url: 'https://product.hstatic.net/200000475003/product/146_559046d3226c4b6b9a0360a7b0c5067a_master.png', model_3d_url: '#' },
-  { id: 6, shop_id: 2, type: 'HAT', gender: 'Female', name: 'Street Snapback Cap', price: 250000, image_url: 'https://images.unsplash.com/photo-1588850561407-ed78c282e89b?auto=format&fit=crop&w=500&q=80', model_3d_url: '#' },
-  { id: 13, shop_id: 2, type: 'TOP', gender: 'Female', name: 'White Linen Shirt', price: 450000, image_url: 'https://images.unsplash.com/photo-1598532163257-ae3c6b2524b6?auto=format&fit=crop&w=500&q=80', model_3d_url: '#' },
-  { id: 7, shop_id: 3, type: 'TOP', gender: 'Female', name: 'White Linen Shirt', price: 450000, image_url: 'https://images.unsplash.com/photo-1598532163257-ae3c6b2524b6?auto=format&fit=crop&w=500&q=80', model_3d_url: '#' },
-  { id: 8, shop_id: 3, type: 'BOTTOM', gender: 'Female', name: 'Beige Trousers', price: 520000, image_url: 'https://images.unsplash.com/photo-1594633312681-425c7b97ccd1?auto=format&fit=crop&w=500&q=80', model_3d_url: '#' },
-  { id: 9, shop_id: 4, type: 'BOTTOM', gender: 'Female', name: 'High Waist Yoga Leggings', price: 350000, image_url: 'https://images.unsplash.com/photo-1506619216599-9d16d0903dfd?auto=format&fit=crop&w=500&q=80', model_3d_url: '#' },
-  { id: 11, shop_id: 5, type: 'TOP', gender: 'Female', name: 'Red Silk Evening Dress', price: 2100000, image_url: 'https://images.unsplash.com/photo-1595777457583-95e059d581b8?auto=format&fit=crop&w=500&q=80', model_3d_url: '#' },
-  { id: 12, shop_id: 5, type: 'ACCESSORY', gender: 'Female', name: 'Pearl Necklace', price: 890000, image_url: 'https://static.malabargoldanddiamonds.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/n/k/nkpjth001_c_1.jpg', model_3d_url: '#' },
-  { id: 14, shop_id: 2, type: 'TOP', gender: 'Male', name: 'Casual Cotton T-Shirt', price: 320000, image_url: 'https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?auto=format&fit=crop&w=500&q=80', model_3d_url: '#',
-}
-
+  { id: 1, shop_id: 1, type: 'TOP', gender: 'Male', name: 'Retro Graphic Tee', sold: 100, rating: 3.4, reviews: 150, price: 350000, image_url: 'https://images.unsplash.com/photo-1583743814966-8936f5b7be1a?auto=format&fit=crop&w=500&q=80', model_3d_url: '#' },
+  { id: 2, shop_id: 1, type: 'BOTTOM', gender: 'Male', name: 'Baggy Jeans 90s', sold: 50, rating: 4.2, reviews: 230, price: 5500000, image_url: 'https://images.unsplash.com/photo-1542272454315-4c01d7abdf4a?auto=format&fit=crop&w=500&q=80', model_3d_url: '#' },
+  { id: 3, shop_id: 1, type: 'SHOES', gender: 'Male', name: 'Classic Chunky Sneakers', sold: 200, rating: 4.5, reviews: 50, price: 1200000, image_url: 'https://images.unsplash.com/photo-1542291026-7eec264c27ff?auto=format&fit=crop&w=500&q=80', model_3d_url: '#' },
+  { id: 4, shop_id: 2, type: 'TOP', gender: 'Male', name: 'Black Oversized Hoodie', sold: 150, rating: 4.1, reviews: 10, price: 650000, image_url: 'https://images.unsplash.com/photo-1556905055-8f358a7a47b2?auto=format&fit=crop&w=500&q=80', model_3d_url: '#' },
+  { id: 5, shop_id: 2, type: 'BOTTOM', gender: 'Male', name: 'Cargo Pants Army Green', sold: 80, rating: 4.3, reviews: 450, price: 550000, image_url: 'https://product.hstatic.net/200000475003/product/146_559046d3226c4b6b9a0360a7b0c5067a_master.png', model_3d_url: '#' },
+  { id: 6, shop_id: 2, type: 'HAT', gender: 'Female', name: 'Street Snapback Cap', sold: 120, rating: 4.6, reviews: 157, price: 250000, image_url: 'https://images.unsplash.com/photo-1588850561407-ed78c282e89b?auto=format&fit=crop&w=500&q=80', model_3d_url: '#' },
+  { id: 7, shop_id: 3, type: 'TOP', gender: 'Female', name: 'White Linen Shirt', sold: 60, rating: 4.2, reviews: 352, price: 450000, image_url: 'https://images.unsplash.com/photo-1598532163257-ae3c6b2524b6?auto=format&fit=crop&w=500&q=80', model_3d_url: '#' },
+  { id: 8, shop_id: 3, type: 'BOTTOM', gender: 'Female', name: 'Beige Trousers', sold: 70, rating: 4.1, reviews: 250, price: 520000, image_url: 'https://images.unsplash.com/photo-1594633312681-425c7b97ccd1?auto=format&fit=crop&w=500&q=80', model_3d_url: '#' },
+  { id: 9, shop_id: 4, type: 'BOTTOM', gender: 'Female', name: 'High Waist Yoga Leggings', sold: 110, rating: 4.7, reviews: 20, price: 350000, image_url: 'https://images.unsplash.com/photo-1506619216599-9d16d0903dfd?auto=format&fit=crop&w=500&q=80', model_3d_url: '#' },
+  { id: 11, shop_id: 5, type: 'TOP', gender: 'Female', name: 'Red Silk Evening Dress', sold: 40, rating: 4.8, reviews: 190, price: 2100000, image_url: 'https://images.unsplash.com/photo-1595777457583-95e05d588b8?auto=format&fit=crop&w=500&q=80', model_3d_url: '#' },
+  { id: 12, shop_id: 5, type: 'ACCESSORY', gender: 'Female', name: 'Pearl Necklace', sold: 3, rating: 4.9, reviews: 178, price: 800000, image_url: 'https://images.unsplash.com/photo-1503342217505-b0a15ec3261c?auto=format&fit=crop&w=500&q=80', model_3d_url: '#' },
+  { id: 13, shop_id: 2, type: 'TOP', gender: 'Female', name: 'White Linen Shirt', sold: 90, rating: 4.4, reviews: 780, price: 450000, image_url: 'https://images.unsplash.com/photo-1598532163257-ae3c6b2524b6?auto=format&fit=crop&w=500&q=80', model_3d_url: '#' },
 ];
 
 export const ORDERS: Order[] = [
